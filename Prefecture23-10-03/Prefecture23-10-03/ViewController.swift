8455508e2f86da320523696830773db806802ef6
shunsuke250 <soeyamashunsuke@gmail.com>
2023-10-04 20:01:27 +0900

    :sparkles: 初期画面のUIの作成およびIBOutlet接続でコードと紐づけ

--- Prefecture23-10-03/Prefecture23-10-03/ViewController.swift
+++ Prefecture23-10-03/Prefecture23-10-03/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var prefectureLabel: UILabel!
+    @IBOutlet private weak var selectButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
