dd9a25a6e3b4f9abda47696a1a7af12319f13a2f
shunsuke250 <soeyamashunsuke@gmail.com>
2023-10-05 00:55:37 +0900

    :sparkles: TableViewを追加

---
+++ Prefecture23-10-03/Prefecture23-10-03/SelectViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SelectViewController.swift
+//  Prefecture23-10-03
+//
+//  Created by 副山俊輔 on 2023/10/05.
+//
+
+import UIKit
+
+class SelectViewController: UIViewController {
+
+    @IBOutlet private weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+}
+
+extension SelectViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        prefectures.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = prefectures[indexPath.row]
+        return cell
+    }
+    
+    
+}
